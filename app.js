a0f5438343225d265771009fecbc0acd6caf0490
Mia Azevedo <mialuzazevedo@gmail.com>
2024-01-24 16:44:11 -0400

    added partials for nav, head and footer

--- app.js
+++ app.js
@@ -6,29 +6,34 @@ app.set('view engine', 'ejs');
 
 app.listen(3000); //listen for requests, localhost is default
 
-app.get('/', (re, res) => {
-  // res.send('<h1>home page</h1>');
-  // res.sendFile('./views/index.html' , {root: __dirname});
-  res.render('index');
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+}); 
+
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', {title: 'Home', blogs});
 });
 
-app.get('/about', (re, res) => {
-  // res.send('<h1>about page</h1>');
-  // res.sendFile('./views/about.html' , {root: __dirname});
-  res.render('about');
+app.get('/about', (req, res) => {
+  res.render('about', {title: 'About'});
 
 });
 
-app.get('/about-me', (re, res) => { //redirect
+app.get('/about-me', (req, res) => { //redirect
   res.redirect('about');
 });
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create');
+  res.render('create', {title: 'Create a new blog'});
 });
 
 app.use((req, res) => { // (needs to be in the bottom) this is like a default in a switch case. the use fire for everry request, but the code is read from to top to bottom. it means that this function will only be fires if none of the code above matches the request. basically for every route not especified this will fire
-  // res.status(404).sendFile('./views/404.html' , {root: __dirname});
-  res.render('404');
+  res.render('404', {title: '404'});
 });
 
