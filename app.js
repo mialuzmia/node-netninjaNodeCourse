d2ae9a49a7e2c78973e1b6a708805eec3ddee08c
Mia Azevedo <mialuzazevedo@gmail.com>
2024-01-29 01:29:05 -0400

    integrated mongoDB

--- app.js
+++ app.js
@@ -1,29 +1,80 @@
-const express = require('express');
+const express = require('express'); 
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blogs');
 
 const app = express(); //express app
 
+const dbURI = 'mongodb+srv://mialuz:7J9lWpJiR2W6HnOB@cluster0.txmrd5g.mongodb.net/node-ninja?retryWrites=true&w=majority'
+
+// mongoose.connect(dbURI)
+//   .then(result => {
+//     app.listen(3000); 
+//     console.log('connected to database')})
+//   .catch(err => console.log(err));
+
+const connectToDB = async () => {
+  try {
+    const result = await mongoose.connect(dbURI);
+    console.log('connected to db');
+    app.listen(3000); 
+    // console.log(result);
+  } catch (err) {
+    console.log(err);
+  };
+};
+connectToDB();
+
+// set view engine
 app.set('view engine', 'ejs');
 
-app.listen(3000); //listen for requests, localhost is default
-
+// middlewares
 app.use('/public', express.static(__dirname + '/public'));
+app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host:', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  console.log('\n');
-  next();
-}); 
+// // mongo sandbox routes
+// app.get('/add-blog', async (req, res) => {
+//   const blog = new Blog({
+//     title: 'new blog 2',
+//     snippet: 'about my new blog',
+//     body: 'more about my new blog'
+//   });
 
+//   try {
+//     const result = await blog.save();
+//     res.json(result);
+//   } catch(err) {
+//     console.log(err);
+//     res.end(err.message);
+//   }
+// });
+
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', {title: 'Home', blogs});
+  res.redirect('/blogs');
+});
+
+app.get('/blogs', async (req, res) => {
+  try {
+    const result = await Blog.find({}).sort({createdAt: -1});
+    res.render('index', { title: 'All Blogs', blogs: result });
+  } catch(err) {
+    res.end(err.message)
+  }
+});
+
+app.get('/blogs/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const result = await Blog.findById(id);
+    res.json(result);
+  } catch(err) {
+    res.end(err.message)
+  }
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', {title: 'Create a new blog'});
 });
 
 app.get('/about', (req, res) => {
@@ -35,11 +86,8 @@ app.get('/about-me', (req, res) => { //redirect
   res.redirect('about');
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', {title: 'Create a new blog'});
-});
 
 app.use((req, res) => { // (needs to be in the bottom) this is like a default in a switch case. the use fire for everry request, but the code is read from to top to bottom. it means that this function will only be fires if none of the code above matches the request. basically for every route not especified this will fire
-  res.render('404', {title: '404'});
+  res.status(404).render('404', {title: '404'});
 });
 
